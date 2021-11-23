378489d558a5a2ea0da7c85ba5199ab4c626d69f
charlesncn <charowcjn@gmail.com>
2021-11-23 07:44:29 +0300

    version 2.1.1.0

---
+++ app/src/main/java/com/e/costingapp/Itemview.java
@@ -0,0 +1,14 @@
+package com.e.costingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Itemview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itemview);
+    }
+}
